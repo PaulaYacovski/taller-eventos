a41d0541ab006b0b3d13a0ba0228430a46638ee5
PaulaYacovski <paula.yacovski@gmail.com>
2024-08-18 01:40:21 -0300

    Update eventos.js
    Evitar la propagación del evento click del botón al contenedor.
    Poner un manejador de eventos para el evento 'click' en el botón y  event.stopPropagation  para parar  la propagación del evento al contenedor

--- eventos.js
+++ eventos.js
@@ -4,4 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
     contenedor.addEventListener('click', function() {
         alert('Hola! Soy el div');
     });
+    const boton = document.querySelector('.contenedor button');
+    
+ boton.addEventListener('click', function(event) {
+        event.stopPropagation();
+        alert('Hola!');
+    });
 });
